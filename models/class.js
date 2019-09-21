aee39692cb318e6ba2f4b15d40bf7c0fb762f993
Kraus, Simon <simon.kraus@aol.com>
2019-09-21 12:24:16 -0500

    models

--- models/class.js
+++ models/class.js
@@ -2,11 +2,11 @@ module.exports = function (sequelize, DataTypes) {
 
     const Class = sequelize.define("Class", {
 
-        id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4
-        },
+        // id: {
+        //     type: DataTypes.UUID,
+        //     primaryKey: true,
+        //     defaultValue: DataTypes.UUIDV4
+        // },
 
         points: {
             type: DataTypes.INTEGER
