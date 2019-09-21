712916ed1c7a3e841b2ce45292005ce76c1503d2
Kraus, Simon <simon.kraus@aol.com>
2019-09-20 20:22:22 -0500

    server works :sparkles: :wrench:

--- server.js
+++ server.js
@@ -18,11 +18,9 @@ app.use(express.static("public"));
 var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the testdb
-if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
-}
+
 // Starting the server, syncing our models ------------------------------------/
-db.Sequelize.sync().then(function () {
+db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
         console.log("==> :earth_americas:  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
     });
