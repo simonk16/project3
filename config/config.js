0600e306dc02124c4db05818ac6db316bd80b813
Kraus, Simon <simon.kraus@aol.com>
2019-09-25 20:43:45 -0500

    set up env

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const env = require("dotenv").config();
+module.exports = {
+  
+    development: {
+      username: "root",
+      password: process.env.DB_PASS,
+      database: "test_db",
+      host: "localhost",
+      dialect: "mysql"
+     
+    }
+}
+
+  
