c7eddac7a77eed7ddb7a8383278e4c6b1889b0af
Kraus, Simon <simon.kraus@aol.com>
2019-09-26 20:58:24 -0500

    seedz

---
+++ seeders/20190926015259-demo-student.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('Students', [{
+    firstName: 'Ben',
+    lastName: 'Flippen',
+    createdAt: new Date(),
+    updatedAt: new Date()
+    
+  }, {
+    firstName: 'Simon',
+    lastName: 'Kraus',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('Students', null, {});
+  }
+};
