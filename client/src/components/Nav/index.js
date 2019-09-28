02612527a5f83fda0d46e8eb2c75ae770893c126
Rachel Givens <rachelgivens@me.com>
2019-09-28 11:03:24 -0500

    nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,7 +6,7 @@ function Nav() {
             <div class="nav-wrapper">
                 <a href="../public/images/logo.png" class="brand-logo center">Logo</a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="#"><img src="../../../public/home.png" border="0"/></a></li>
+                    <li><a href="#"><img src="images/home.png" border="0"/></a></li>
                     <li><a href="#"><img src="../../../public/images/exit.png" border="0" /></a></li>
                     <li><a href="collapsible.html">JavaScript</a></li>
                 </ul>
