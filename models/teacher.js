b207bab05e00ba9efeb89b12804babaf90353fe9
Kraus, Simon <simon.kraus@aol.com>
2019-09-28 16:55:28 -0500

    all routes

--- models/teacher.js
+++ models/teacher.js
@@ -25,8 +25,6 @@ module.exports = function(sequelize, DataTypes) {
     });
   
     Teacher.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
       Teacher.belongsTo(models.Class, {onDelete: "cascade"});
     };
   
