d0567362aec17040725c6eaec1fe511c58fde759
Rachel Givens <rachelgivens@me.com>
2019-10-02 23:46:47 -0500

    added card sign up andand changed card sign in

--- client/src/components/CardSignIn/index.js
+++ client/src/components/CardSignIn/index.js
@@ -1,18 +1,50 @@
 import React from "react";
+import Axios from "axios"
+import {Redirect} from "react-router-dom"
 import './style.css';
 import { Header, Input, TextArea, FormBtn } from '../Form/index.js';
 
+class CardSignIn extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: "",
+            password: "",
+            loggedIn: false
+        }
+    }
 
-function CardSignIn() {
-    return (
-        <div className="userCard">
-            <Header />
-            <Input />
-            <TextArea />
-            <br></br>
-            <FormBtn />
-        </div>
-    );
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    loginUser = (event) => {
+        event.preventDefault();
+        Axios.post("/loginUser", {
+            username: this.state.username,
+            password: this.state.password,
+        }).then((res) => {
+            localStorage.setItem('JWT', res.data.token);
+            this.setState({ loggedIn: true })
+            console.log("logged in")
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    render () {
+        return (
+            <div className="userCard">
+                <Header />
+                <Input />
+                <TextArea />
+                <br></br>
+                <FormBtn />
+            </div>
+        );
+    }
 }
 
 export default CardSignIn;
