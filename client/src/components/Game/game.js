de940a9f6bee9971a52631db6d6e3800f03d1ccf
Flippen, Ben <benflippen@gmail.com>
2019-10-03 14:55:55 -0500

    game.js randomizes students from the students table

--- client/src/components/Game/game.js
+++ client/src/components/Game/game.js
@@ -3,7 +3,8 @@ import API from '../../utils/API'
 
 export default class game extends Component {
     state = {
-        students: []
+        students: [],
+        randomStudent: ""
     }
     componentDidMount() {
         this.getStudents();
@@ -14,15 +15,30 @@ export default class game extends Component {
         this.setState({
             students: response.data
         })
+        console.log("line 17: " + this.state.students)
+    } 
+
+    randomSelect = () => {
+        let studentsArr = this.state.students;
+        const keys = Object.keys(studentsArr);
+        const randomIndex = keys[Math.floor(Math.random() * keys.length)];
+        const item = studentsArr[randomIndex];
+        console.log("line 26 code on game.js has chosen " + item.firstName)
+        let oldState = this.state
+        oldState.randomStudent = item.firstName
+        this.setState(oldState)
+        
     }
+
+
     render() {
         return (
             <div>
                 <div class="card w-75">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Button</a>
+                        <h5 class="card-title">Click to button to select a Student!</h5>
+                        <p class="card-text">{this.state.randomStudent}</p>
+                        <a href="#" class="btn btn-primary" onClick={this.randomSelect}>Button</a>
                     </div>
                 </div>
             </div>
